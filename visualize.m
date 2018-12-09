a133a7cc34e5ba048bb2ccb70c9ce865b4e906c0
xihajun <www.junfan.com@gmail.com>
2018-12-10 06:16:58 +0800

    Update visualize.m

--- visualize.m
+++ visualize.m
@@ -4,6 +4,8 @@ pause(5)
 for i =1:6
     figure(i)
     T=path{list(i)}
+    plot(ceil(T(1)/100),mod(T(1),100),'go','MarkerFaceColor','r');
+    plot(ceil(T(length(T))/100),mod(T(length(T)),100),'go','MarkerFaceColor','r');
     for i =1:length(T)
         plot(ceil(T(i)/100),mod(T(i),100),'ro','MarkerFaceColor','r');
         pause(1)
